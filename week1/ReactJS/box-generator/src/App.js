7525ae593bb5e2e62764437de99acf00671723b6
oggyman <oggyla@gmail.com>
2020-05-07 18:45:09 -0700

    updated App.js to work wiht Box.js

--- week1/ReactJS/box-generator/src/App.js
+++ week1/ReactJS/box-generator/src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Box from './components/Box/Box';
+
 
 function App() {
+  const [color, setColor] = useState("");
+  const [height, setHeight] = useState("");
+  const [width, setWidth] = useState("");
+  const [colors, setColors] = useState([]);
+  
+  const form = e => {
+    e.preventDefault()
+    const array = [...colors]
+    array.push({
+      color : e.target.color.value, 
+      height : e.target.height.value,
+      width : e.target.width.value
+    })
+    setColors(array)
+    setColor("")
+    console.log(colors)
+    console.log(e.target.color.value)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={form}>
+        <div>
+          <label htmlFor="color">Color: </label>
+          <input type="text" name="color" onChange={ e => setColor(e.target.value)} value={color}/>
+        </div>
+        <div>
+          <label htmlFor="width">Width: </label>
+          <input type="text" name="width"/>
+        </div>
+        <div>
+          <label htmlFor = "height">Height: </label>
+          <input type="text" name="height"/>
+        </div>
+        <button type="submit">Add Color</button>
+
+      </form>
+        {
+            colors.map( (item, i) => 
+                <Box params = { item } key = {i}/>
+            )
+        }
+
     </div>
   );
 }
