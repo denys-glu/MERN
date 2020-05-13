4500dab727873f61c265202f8ed7efe62f6bcbed
oggyman <oggyla@gmail.com>
2020-05-13 12:36:57 -0700

    fixed Address object definition, added routes

--- Express/Faker_API/server.js
+++ Express/Faker_API/server.js
@@ -6,11 +6,11 @@ class Company {
     constructor() {
         this.name = faker.company.companyName();
         this.address = {
-            street = faker.address.streetAddress(),
-            city = faker.address.city(),
-            state = faker.address.state(),
-            zipCode = faker.address.zipCode(),
-            country = faker.address.country()
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
         }
     }
 }
@@ -27,8 +27,14 @@ class User {
 
 // req is short for request
 // res is short for response
-app.get("/api", (req, res) => {
-    res.send("Our express api server is now sending this over to the browser");
+app.get("/api/users/new", (req, res) => {
+    res.json( new User );
+});
+app.get("/api/companies/new", (req, res) => {
+    res.json( new Company );
+});
+app.get("/api/user/company", (req, res) => {
+    res.json( {user: new User, company: new Company} );
 });
 
 const server = app.listen(8000, () =>
