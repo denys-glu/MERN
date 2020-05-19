1d09260da6a462962a615c5d90359531e132740e
oggyman <oggyla@gmail.com>
2020-05-19 10:48:33 -0700

    added basic validation to Author Schema

--- FullStack/Authors/server/models/author.model.js
+++ FullStack/Authors/server/models/author.model.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
 const AuthorSchema = new mongoose.Schema({
-        firstName: String,
-        lastName: String
-    });
+    firstName: {
+        type: String,
+        required: [true, "First Name is required!"],
+        minlength: [3, "First Name should be longer than 2 characters!"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last Name is required!"],
+        minlength: [3, "Last Name should be longer than 2 characters!"]
+    }
+}, { tomestamp: true });
 
 const Author = mongoose.model("Author", AuthorSchema);
 
