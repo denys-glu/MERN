4e9a80c20f9f77715b35ce3450731f8262fea11a
oggyman <oggyla@gmail.com>
2020-05-20 20:56:21 -0700

    added delete button as a modal

--- FullStack/TeamManager/client/src/components/DeleteButton.js
+++ FullStack/TeamManager/client/src/components/DeleteButton.js
@@ -4,9 +4,25 @@ function DeleteButton({ deleteHandler, item }) {
 
     return (
         <>
-            <button className="btn btn-danger" onClick={e => deleteHandler(item._id)} className="btn btn-danger">
-                Delete
-            </button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target={`#myModal${item._id}`}>Delete Player {item.name}</button>
+
+            <div id={`myModal${item._id}`} className="modal fade bd-example-modal-sm" 
+                                            tabIndex="-1" 
+                                            role="dialog"
+                                            aria-labelledby="mySmallModalLabel" 
+                                            aria-hidden="true">
+                <div className="modal-dialog modal-sm modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-body">
+                            <p>Are you sure you want to delete<br/> <strong>{item.name}</strong></p>
+                            <button type="button" className="btn btn-danger" onClick={e => deleteHandler(item._id)} data-dismiss="modal">
+                                Delete
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
